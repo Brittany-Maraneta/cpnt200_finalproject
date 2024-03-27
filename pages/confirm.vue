928daca4921f322957c345a5ab1b6c1506d1fa86
DKH <derek.hawryluk@edu.sait.ca>
2024-03-27 13:54:19 -0600

    page org

--- pages/confirm.vue
+++ pages/confirm.vue
@@ -1,3 +1,16 @@
+<template>
+  <h1>Roaming Routes</h1>
+  <div>
+    <ul>
+      <li v-for="Roaming_Routes in roaming" :key="Roaming_Routes.id">
+        <h3>{{ Roaming_Routes.blog_title }}</h3>
+        {{ Roaming_Routes.blog_text }}
+        {{ Roaming_Routes.comment }}
+      </li>
+    </ul>
+  </div>
+</template>
+
 <script setup lang="ts">
 const user = useSupabaseUser();
 
@@ -10,8 +23,15 @@ watch(
   },
   { immediate: true }
 );
+const client = useSupabaseClient();
+const { data: roaming } = await useAsyncData("Roaming_Routes", async () => {
+  try {
+    let { data, error } = await client
+      .from("Roaming_Routes")
+      .select("blog_title, blog_text");
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+});
 </script>
-
-<template>
-  <h1>Roaming Routes</h1>
-</template>
--- pages/index.vue
+++ pages/index.vue
@@ -5,29 +5,11 @@
       <NuxtLink to="/login">Login</NuxtLink>
     </h1>
   </div>
-  <div>
-    <ul>
-      <li v-for="Roaming_Routes in roaming" :key="Roaming_Routes.id">
-        <h3>{{ Roaming_Routes.blog_title }}</h3>
-        {{ Roaming_Routes.blog_text }}
-        {{ Roaming_Routes.comment }}
-      </li>
-    </ul>
-  </div>
+
 </template>
 
 <script setup>
-const client = useSupabaseClient();
-const { data: roaming } = await useAsyncData("Roaming_Routes", async () => {
-  try {
-    let { data, error } = await client
-      .from("Roaming_Routes")
-      .select("blog_title, blog_text");
-    return data;
-  } catch (error) {
-    console.error(error);
-  }
-});
+
 </script>
 
 <style>
